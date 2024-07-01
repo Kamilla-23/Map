70700fac0e0901c7633458cba02a6f361afb4363
Kamilla <s_nauruzbayeva23@stud.hwr-berlin.de>
2024-07-01 16:47:53 +0200

    Removed timer and added interface changes

--- berlin-map/app/app.py
+++ berlin-map/app/app.py
@@ -3,7 +3,6 @@ import geopandas as gpd
 import folium
 from streamlit_folium import st_folium
 from folium.plugins import HeatMap
-import time
 from pathlib import Path
 import json
 from pyproj import Transformer
@@ -11,9 +10,6 @@ import pandas as pd
 import get_crime
 
 st.set_page_config(layout="wide")
-
-# Measure total runtime
-start_total = time.time()
 
 # Cache the function for loading district boundaries
 @st.cache_data
@@ -42,42 +38,32 @@ def load_traffic_data(file_path):
 def load_police_precincts(file_path):
     return gpd.read_file(file_path)
 
-# Measure time for loading district boundaries
-start_districts = time.time()
+# Load district boundaries
 districts_file = Path(__file__).parent / 'bezirksgrenzen.geojson'
 districts = load_districts(districts_file)
-end_districts = time.time()
 
-# Measure time for loading streetlight GeoJSON
-start_streetlights = time.time()
+# Load streetlight GeoJSON
 streetlights_file = Path(__file__).parent / 'pruned_streetlight.geojson'
 streetlights = load_streetlights(streetlights_file)
-end_streetlights = time.time()
 
-# Measure time for loading traffic data GeoJSON
-start_traffic = time.time()
+# Load traffic data GeoJSON
 traffic_file = Path(__file__).parent / 'converted_telraam_segments.geojson'
 traffic_coordinates = load_traffic_geojson(traffic_file)
-end_traffic = time.time()
 
-# Measure time for loading segment traffic data
-start_segment_data = time.time()
+# Load segment traffic data
 segment_file = Path(__file__).parent / 'fetched_segments_data.json'
 segment_data = load_traffic_data(segment_file)
-end_segment_data = time.time()
 
-# Measure time for loading police precincts GeoJSON
-start_police_precincts = time.time()
+# Load police precincts GeoJSON
 police_precincts_file = Path(__file__).parent / 'converted_police_precincts.geojson'
 police_precincts = load_police_precincts(police_precincts_file)
-end_police_precincts = time.time()
 
 crime_data = get_crime.load_and_process_crime_data()
 
 # Streamlit sidebar options
 st.sidebar.title("Map Options")
 selected_district = st.sidebar.selectbox("Choose a District", districts['Gemeinde_name'].unique())
-selected_layer = st.sidebar.selectbox("Select Layer", ["Streetlights", "Traffic Data", "Police Precincts", "Crime Data", "Crime Heat Map"])
+selected_layer = st.sidebar.selectbox("Select Layer", ["Crime Heat Map", "Crime Data", "Traffic Data", "Streetlights"])
 
 # Add descriptions for each layer
 layer_descriptions = {
@@ -85,7 +71,7 @@ layer_descriptions = {
     "Traffic Data": "This layer displays traffic data, including average counts of cars, bikes, and pedestrians per hour for different segments.",
     "Police Precincts": "This layer highlights the locations and contact information of police precincts in the district.",
     "Crime Data": "This layer presents detailed crime data for the selected year and crime type.",
-    "Crime Heat Map": "This layer shows a heat map of total crime incidents across the district."
+    "Crime Heat Map": "This layer shows a heat map of crime incidents across all districts based on selected crime type and year."
 }
 
 st.sidebar.write(layer_descriptions[selected_layer])
@@ -93,19 +79,42 @@ st.sidebar.write(layer_descriptions[selected_layer])
 district = districts[districts['Gemeinde_name'] == selected_district]
 
 if selected_layer == "Traffic Data":
-    selected_hour = st.sidebar.selectbox("Choose an Hour", list(range(24)))
+    selected_hour = st.sidebar.slider("Choose an Hour", min_value=0, max_value=23)
 
 if selected_layer == "Crime Data" or selected_layer == "Crime Heat Map":
     crime_data_in_district = crime_data[crime_data['Gemeinde_name'] == selected_district]
     if crime_data_in_district.empty:
         st.error(f"No crime data available for {selected_district}")
     else:
-        selected_year = st.sidebar.selectbox("Choose a Year", crime_data_in_district["Jahr"].unique())
+        #selected_year = st.sidebar.selectbox("Choose a Year", crime_data_in_district["Jahr"].unique())
+        selected_year = st.sidebar.slider("Choose a Year", int(crime_data_in_district["Jahr"].min()), int(crime_data_in_district["Jahr"].max()), step=1)
 
         if selected_layer == "Crime Heat Map":
-            selected_crime_type = st.sidebar.selectbox("Choose a Crime Type", crime_data.columns.drop(['Gemeinde_name', 'Jahr','geometry']))
+            # Create aliases
+            crime_type_aliases = {
+            'Raub': 'Robbery',
+            'Straßenraub': 'Burglary',
+            'Körperverletzung': 'Assault',
+            'schwere Körperverletzung': 'Serious assult',
+            'Diebstahl': 'Theft',
+            'Freiheitsberaubung': 'Deprivation of Liberty',
+            'Other': 'Other crimes',
+            'Gesamt': 'All'
+            }           
 
-start_map_creation = time.time()
+            # Reverse mapping
+            alias_to_crime_type = {v: k for k, v in crime_type_aliases.items()}
+
+            # Create a list of English aliases for the selectbox
+            crime_types = [crime_type_aliases[col] for col in crime_data.columns.drop(['Gemeinde_name', 'Jahr', 'geometry'])]
+
+            # Add selectbox to sidebar with English aliases
+            selected_crime_type_alias = st.sidebar.selectbox("Choose a Crime Type", crime_types)
+
+            # Get the corresponding German column name
+            selected_crime_type = alias_to_crime_type[selected_crime_type_alias]
+
+# Create a base map
 
 if selected_layer == "Crime Heat Map":
     bounds = districts.total_bounds 
@@ -123,39 +132,49 @@ else:
     )
     folium.GeoJson(district).add_to(m)
 
-end_map_creation = time.time()
 
 # Add the selected layer to the map
-if selected_layer == "Streetlights":
-    # Measure time for filtering streetlights within the selected district
-    start_filter_streetlights = time.time()
-    streetlights_in_district = streetlights[streetlights.geometry.within(district.geometry.squeeze())]
-    end_filter_streetlights = time.time()
+if selected_layer == "Crime Heat Map":
+    
+    # Prepare data for the heat map
+    filtered_crime_data = crime_data[(crime_data["Jahr"] == selected_year) & (crime_data[selected_crime_type] > 0)]
 
-    # Measure time for adding streetlights to the map
-    start_add_streetlights = time.time()
-    for _, row in streetlights_in_district.iterrows():
-        for point in row.geometry.geoms: 
-            folium.CircleMarker(
-                location=[point.y, point.x],
-                radius=0.5,
-                color='yellow',
-                fill=True,
-                fill_color='yellow'
-            ).add_to(m)
-    end_add_streetlights = time.time()
+    # Prepare data for the heat map with selected crime type
+    heat_data = [[row['geometry'].centroid.y, row['geometry'].centroid.x, row[selected_crime_type]] for idx, row in filtered_crime_data.iterrows()]
+
+    # Add heat map to the map
+    HeatMap(heat_data).add_to(m)
+
+elif selected_layer == "Crime Data":
+
+    crime_data_in_district = crime_data[(crime_data['Gemeinde_name'] == selected_district) & (crime_data['Jahr'] == selected_year)]
+    
+    # Add crime data to the map
+    for _, row in crime_data_in_district.iterrows():
+        coords = row['geometry'].centroid.coords[0][::-1]  
+        popup_html = f"<b>Crime Data ({row['Jahr']})</b><br>"
+        popup_html += f"<b>Total Crimes:</b> {row['Gesamt']}<br>"
+        popup_html += f"<b>Robbery:</b> {row['Raub']}<br>"
+        popup_html += f"<b>Street Robbery:</b> {row['Straßenraub']}<br>"
+        popup_html += f"<b>Assault:</b> {row['Körperverletzung']}<br>"
+        popup_html += f"<b>Serious Assault:</b> {row['schwere Körperverletzung']}<br>"
+        popup_html += f"<b>Deprivation of Liberty:</b> {row['Freiheitsberaubung']}<br>"
+        popup_html += f"<b>Theft:</b> {row['Diebstahl']}<br>"
+        popup_html += f"<b>Other Crimes:</b> {row['Other']}<br>"
+        
+        folium.Marker(
+            location=coords,
+            popup=folium.Popup(popup_html, max_width=500),
+            icon=folium.Icon(color='green', icon='info-sign')
+        ).add_to(m)
 
 elif selected_layer == "Traffic Data":
-    # Measure time for filtering traffic segments within the selected district
-    start_filter_traffic = time.time()
+
     traffic_segments_in_district = [
         feature for feature in traffic_coordinates['features']
         if any([district.geometry.squeeze().contains(gpd.points_from_xy([coord[0]], [coord[1]])[0]) for coord in feature['geometry']['coordinates']])
     ]
-    end_filter_traffic = time.time()
 
-    # Measure time for adding traffic segments to the map
-    start_add_traffic = time.time()
     for feature in traffic_segments_in_district:
         coordinates = feature['geometry']['coordinates']
         segment_id = str(feature['properties']['segment_id']) 
@@ -185,16 +204,10 @@ elif selected_layer == "Traffic Data":
                         max_width=500
                     )
                 ).add_to(m)
-    end_add_traffic = time.time()
 
 elif selected_layer == "Police Precincts":
 
-    start_filter_police_precincts = time.time()
     police_in_district = police_precincts[police_precincts.geometry.within(district.geometry.squeeze())]
-    end_filter_police_precincts = time.time()
-
-    # Measure time for adding police precincts to the map
-    start_add_police_precincts = time.time()
 
     for _, precinct in police_in_district.iterrows():
         popup_html = f"<b>Police Precinct</b><br>"
@@ -213,52 +226,20 @@ elif selected_layer == "Police Precincts":
             icon=folium.Icon(color='red', icon='shield')
         ).add_to(m)
 
-    end_add_police_precincts = time.time()
+elif selected_layer == "Streetlights":
 
-elif selected_layer == "Crime Data":
-    # Measure time for adding crime data to the map
-    start_add_crime_data = time.time()
+    streetlights_in_district = streetlights[streetlights.geometry.within(district.geometry.squeeze())]
 
-    crime_data_in_district = crime_data[(crime_data['Gemeinde_name'] == selected_district) & (crime_data['Jahr'] == selected_year)]
-    
-    # Add crime data to the map
-    for _, row in crime_data_in_district.iterrows():
-        coords = row['geometry'].centroid.coords[0][::-1]  
-        popup_html = f"<b>Crime Data ({row['Jahr']})</b><br>"
-        popup_html += f"<b>Total Crimes:</b> {row['Gesamt']}<br>"
-        popup_html += f"<b>Robbery:</b> {row['Raub']}<br>"
-        popup_html += f"<b>Street Robbery:</b> {row['Straßenraub']}<br>"
-        popup_html += f"<b>Assault:</b> {row['Körperverletzung']}<br>"
-        popup_html += f"<b>Serious Assault:</b> {row['schwere Körperverletzung']}<br>"
-        popup_html += f"<b>Deprivation of Liberty:</b> {row['Freiheitsberaubung']}<br>"
-        popup_html += f"<b>Theft:</b> {row['Diebstahl']}<br>"
-        popup_html += f"<b>Other Crimes:</b> {row['Other']}<br>"
-        
-        folium.Marker(
-            location=coords,
-            popup=folium.Popup(popup_html, max_width=500),
-            icon=folium.Icon(color='green', icon='info-sign')
-        ).add_to(m)
-
-    end_add_crime_data = time.time()
-elif selected_layer == "Crime Heat Map":
-    # Measure time for adding crime heat map to the map
-    start_add_heat_map = time.time()
-    
-    # Prepare data for the heat map
-    filtered_crime_data = crime_data[(crime_data["Jahr"] == selected_year) & (crime_data[selected_crime_type] > 0)]
-
-    # Prepare data for the heat map with selected crime type
-    heat_data = [[row['geometry'].centroid.y, row['geometry'].centroid.x, row[selected_crime_type]] for idx, row in filtered_crime_data.iterrows()]
-
-    # Add heat map to the map
-    HeatMap(heat_data).add_to(m)
-
-    end_add_heat_map = time.time()
+    for _, row in streetlights_in_district.iterrows():
+        for point in row.geometry.geoms: 
+            folium.CircleMarker(
+                location=[point.y, point.x],
+                radius=0.5,
+                color='yellow',
+                fill=True,
+                fill_color='yellow'
+            ).add_to(m)
 
 # Display the map with Streamlit
 st.title("Berlin Map")
 st_folium(m, width=1400, height=1000)
-
-# Measure total end time
-end_total = time.time()
